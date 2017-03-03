4689809a07c8efb9e0d0640f746206e025c18ee3
Abdusamad <ZKWFZPJ@corp.bankofamerica.com>
2017-03-03 15:26:37 -0600

    initial commit

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service';
+
+@Component({
+	selector: 'my-hero-detail',
+	template: `
+		<div *ngIf="hero">
+			<h2>{{hero.name}} details!</h2>
+			<div><label>id: </label>{{hero.id}}</div>
+			<div>
+				<label>name: </label>
+				<input [(ngModel)]="hero.name" placeholder="name" />
+			</div>
+		</div>
+		`
+})
+
+export class HeroDetailComponent{
+	@Input() hero: Hero;
+
+	constructor(
+		private heroService: HeroService,
+		private route: ActivatedRoute,
+		private location: Location
+	){}
+}
